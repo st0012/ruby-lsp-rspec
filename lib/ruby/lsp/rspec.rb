a928249b303f7cdc3e19ef1cfff98f28c07db790
Stan Lo <stan.lo@shopify.com>
2023-09-10 22:08:13 +0100

    Initial commit

---
+++ lib/ruby/lsp/rspec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rspec/version"
+
+module Ruby
+  module Lsp
+    module Rspec
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/ruby/lsp/rspec_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruby::Lsp::Rspec do
+  it "has a version number" do
+    expect(Ruby::Lsp::Rspec::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

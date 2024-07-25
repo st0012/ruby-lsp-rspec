b1ec625c49ce414e86107f202aba7f8998e2262b
Stan Lo <stan001212@gmail.com>
2024-07-25 21:22:28 +0100

    Support go-to-definition for let(!) and subject declaration

--- spec/ruby_lsp_rspec_spec.rb
+++ spec/ruby_lsp_rspec_spec.rb
@@ -1,10 +1,211 @@
 # typed: false
 # frozen_string_literal: true
 
+require "tempfile"
+
 RSpec.describe RubyLsp::RSpec do
   include RubyLsp::TestHelper
 
   let(:uri) { URI("file:///fake_spec.rb") }
+
+  describe "definition" do
+    it "finds the subject declaration" do
+      source = <<~RUBY
+        RSpec.describe Foo do
+          subject { 1 }
+
+          it "does something" do
+            subject
+            foo(subject)
+          end
+        end
+      RUBY
+
+      tempfile = Tempfile.new
+      tempfile.write(source)
+      tempfile.close
+      uri = URI(tempfile.path)
+
+      with_server(source, uri) do |server, uri|
+        index = server.instance_variable_get(:@global_state).index
+        index.index_single(RubyIndexer::IndexablePath.new(nil, tempfile.path))
+        server.process_message(
+          {
+            id: 1,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 4, character: 4 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(1)
+        expect(response[0].target_uri).to eq(URI::Generic.from_path(path: tempfile.path).to_s)
+        range = response[0].target_range.attributes
+        range_hash = { start: range[:start].to_hash, end: range[:end].to_hash }
+        expect(range_hash).to eq(
+          start: { line: 1, character: 10 },
+          end: { line: 1, character: 15 },
+        )
+
+        server.process_message(
+          {
+            id: 2,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 5, character: 9 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(1)
+        expect(response[0].target_uri).to eq(URI::Generic.from_path(path: tempfile.path).to_s)
+        range = response[0].target_range.attributes
+        range_hash = { start: range[:start].to_hash, end: range[:end].to_hash }
+        expect(range_hash).to eq(
+          start: { line: 1, character: 10 },
+          end: { line: 1, character: 15 },
+        )
+      end
+    ensure
+      tempfile&.unlink
+    end
+
+    it "finds named subject declaration" do
+      source = <<~RUBY
+        RSpec.describe Foo do
+          subject(:variable) { 1 }
+
+          it "does something" do
+            subject
+            foo(variable)
+          end
+        end
+      RUBY
+
+      tempfile = Tempfile.new
+      tempfile.write(source)
+      tempfile.close
+      uri = URI(tempfile.path)
+
+      with_server(source, uri) do |server, uri|
+        index = server.instance_variable_get(:@global_state).index
+        index.index_single(RubyIndexer::IndexablePath.new(nil, tempfile.path))
+        server.process_message(
+          {
+            id: 1,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 4, character: 4 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(0)
+
+        server.process_message(
+          {
+            id: 2,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 5, character: 9 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(1)
+        expect(response[0].target_uri).to eq(URI::Generic.from_path(path: tempfile.path).to_s)
+        range = response[0].target_range.attributes
+        range_hash = { start: range[:start].to_hash, end: range[:end].to_hash }
+        expect(range_hash).to eq(
+          start: { line: 1, character: 21 },
+          end: { line: 1, character: 26 },
+        )
+      end
+    ensure
+      tempfile&.unlink
+    end
+
+    it "finds the let declaration" do
+      source = <<~RUBY
+        RSpec.describe Foo do
+          let(:variable) { 1 }
+
+          it "does something" do
+            variable
+            foo(variable)
+          end
+        end
+      RUBY
+
+      tempfile = Tempfile.new
+      tempfile.write(source)
+      tempfile.close
+      uri = URI(tempfile.path)
+
+      with_server(source, uri) do |server, uri|
+        index = server.instance_variable_get(:@global_state).index
+        index.index_single(RubyIndexer::IndexablePath.new(nil, tempfile.path))
+        server.process_message(
+          {
+            id: 1,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 4, character: 4 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(1)
+        expect(response[0].target_uri).to eq(URI::Generic.from_path(path: tempfile.path).to_s)
+        range = response[0].target_range.attributes
+        range_hash = { start: range[:start].to_hash, end: range[:end].to_hash }
+        expect(range_hash).to eq(
+          start: { line: 1, character: 17 },
+          end: { line: 1, character: 22 },
+        )
+
+        server.process_message(
+          {
+            id: 2,
+            method: "textDocument/definition",
+            params: {
+              textDocument: { uri: uri },
+              position: { line: 5, character: 9 },
+            },
+          },
+        )
+
+        response = server.pop_response.response
+
+        expect(response.count).to eq(1)
+        expect(response[0].target_uri).to eq(URI::Generic.from_path(path: tempfile.path).to_s)
+        range = response[0].target_range.attributes
+        range_hash = { start: range[:start].to_hash, end: range[:end].to_hash }
+        expect(range_hash).to eq(
+          start: { line: 1, character: 17 },
+          end: { line: 1, character: 22 },
+        )
+      end
+    ensure
+      tempfile&.unlink
+    end
+  end
 
   describe "document symbol" do
     it "generates correct document symbols" do

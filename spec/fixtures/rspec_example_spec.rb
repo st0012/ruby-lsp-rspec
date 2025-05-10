a4322aaaf88d89cea94cf292af28d9c83619b905
Stan Lo <stan001212@gmail.com>
2025-05-10 15:41:58 +0100

    Add tests for the rspec formatter

---
+++ spec/fixtures/rspec_example_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# TODO: Ideally, these should be done in rspec_formatter_spec.rb's popen3 call
+# but for some reason the `-r` option doesn't correctly load the formatter
+require_relative "../../lib/ruby_lsp/ruby_lsp_rspec/rspec_formatter"
+
+RSpec.configure do |config|
+  config.formatter = "RubyLsp::RSpec::RSpecFormatter"
+end
+
+RSpec.describe "RSpecExample" do
+  describe "A sample test group" do
+    it "passes" do
+      expect(1).to eq(1)
+    end
+
+    it "fails" do
+      expect(2).to eq(1)
+    end
+
+    it "is pending" do
+      pending
+      expect(true).to be(false)
+    end
+
+    it do
+      expect(1).to eq(1)
+    end
+
+    it "raises an error" do
+      raise "oops"
+    end
+  end
+end

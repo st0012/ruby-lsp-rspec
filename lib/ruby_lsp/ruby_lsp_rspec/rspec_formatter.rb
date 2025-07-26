f0a9f48a84e325e84a08522b1e836b0ad3edc903
Michiel Hendriks <elmuerte@users.noreply.github.com>
2025-07-26 14:04:35 +0200

    Fix testing with coverage (#81)
    * Notify coverage gathering is done
    
    When testing with coverage RubyLsp::LspReporter.instance.shutdown will
    not actually shutdown. It expects to be notified via at_coverage_exit
    so that it can gather coverage stats.
    
    * Either shutdown or coverage_exit depending on coverage status
    
    Co-authored-by: Stan Lo <stan001212@gmail.com>
    
    ---------
    
    Co-authored-by: Stan Lo <stan001212@gmail.com>

--- lib/ruby_lsp/ruby_lsp_rspec/rspec_formatter.rb
+++ lib/ruby_lsp/ruby_lsp_rspec/rspec_formatter.rb
@@ -61,7 +61,11 @@ module RubyLsp
       end
 
       def stop(notification)
-        RubyLsp::LspReporter.instance.shutdown
+        if RubyLsp::LspReporter.start_coverage?
+          RubyLsp::LspReporter.instance.at_coverage_exit
+        else
+          RubyLsp::LspReporter.instance.shutdown
+        end
       end
 
       def uri_for(example)
